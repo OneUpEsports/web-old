1f3179a2b71fc6cdc42f02c5463bb2a1e17047eb
nid_ch <ne.chenni@gmail.com>
2021-12-22 19:56:33 +0100

    fixing url for register

---
+++ src/pages/subscribe.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { navigate } from "gatsby"
+import SocialLinks from "../../components/subscribe-comps/socialLinks"
+import OrBanner from "../../components/subscribe-comps/orBanner"
+const Subscribe = () => {
+
+    // Styles
+    const containerStyle=" max-w-xl  text-white m-auto text-center justify-items-center  justify-center items-center"
+    const mainTitleStyle="max-w-md text-center m-auto"
+    const socialLinksBoxStyle="buttons min-w-fit  m-auto justify-items-center  justify-center items-center"
+    const socialLinksTitleStyle="text-center text-base mb-2"
+    const signUpBtnStyle="bg-indigo-900 rounded block text-white w-64 h-10 mt-2 mb-4 text-sm font-bold  m-auto "
+   
+   
+    return (
+        
+        <div className="absolute bottom-0 right-0  bg-black m-0 h-full  w-full items-center py-24 m-0 p-0">
+             <div className={containerStyle}>
+                 <h2 className={mainTitleStyle}>Be a part of the next Gaming Adventure</h2>
+                 <br/><br/>
+                 <div className={socialLinksBoxStyle}>
+                     <h3 className={socialLinksTitleStyle}>Get an Account with</h3>
+                     <SocialLinks/>
+                 </div>
+                 <br/>
+                 <OrBanner/>
+        
+                  <div>
+                     <button onClick={() => navigate("/register/email")} className={signUpBtnStyle}>CONTINUE WITH EMAIL</button>
+                     <h4 className="text-gray-400 text-sm font-bold">Already have an account?  <a className="text-white cursor-pointer"  onClick={() => navigate("/login")}>Log in</a></h4>
+                  </div>
+             </div>
+        </div>
+        
+        
+
+     
+     
+    );
+}
+export default Subscribe;
