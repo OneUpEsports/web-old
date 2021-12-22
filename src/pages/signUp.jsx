4d8529274a1fa84a31182c99128a46c09a5e09ec
CHENNI-Nidhaleddine <75954258+CHENNI-Nidhaleddine@users.noreply.github.com>
2021-12-22 19:15:32 +0100

    Update signUp.jsx

--- src/pages/signUp.jsx
+++ src/pages/signUp.jsx
@@ -14,7 +14,7 @@ const SignUp = () => {
     
     return (
         <>
-        <div className="absolute bottom-0 right-0 bg-black m-0 h-full w-full items-center py-24">
+        <div className=" bottom-0 right-0 bg-black m-0 h-full w-full items-center py-24">
             <div className={containerStyle}>
                 <h3 className={mainTitleStyle}>Subscribe with email</h3>
                 <form className="relative my-4 ">
