9fb7a9d89f9e887ee69c76d841f1eaba4f884a45
CHENNI-Nidhaleddine <75954258+CHENNI-Nidhaleddine@users.noreply.github.com>
2021-12-23 14:45:30 +0100

    Update login.jsx

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -5,17 +5,17 @@ import InputField from "../components/subscribe-comps/inputField";
 const Login = () => {
     // Styles
     const submitStyle =
-        "cursor-pointer bg-indigo-900 rounded block text-white w-96 h-10 mt-2 mb-4 text-sm font-bold  m-auto ";
+        "cursor-pointer bg-indigo-900 rounded block text-center text-white w-72 h-10 mt-2 mb-4 text-sm font-bold  m-auto ";
     const titleStyle = "text-center  mb-2";
     const linkStyle = "text-gray-400 text-sm font-bold mb-1";
     const SocialLinksStyle =
-        "min-w-fit  m-auto justify-items-center  justify-center items-center";
+        "min-w-fit  m-auto justify-items-center  justify-center items-center mb-24";
     const containerStyle =
         " max-w-xl  text-white m-auto my-auto text-center justify-items-center  justify-center items-center";
 
     return (
         <>
-            <div className=" absolute bottom-0 right-0 bg-black m-0 h-full w-full  items-center mb-0 pb-0 py-24 overflow-x-hidden overflow-y-scroll">
+            <div className=" absolute bottom-0 right-0 bg-black m-0 h-full w-full  items-center mb-0 pb-0 py-24  overflow-x-hidden overflow-y-scroll">
                 <div className={containerStyle}>
                     <h3 className={titleStyle}>Login with</h3>
                     <h4 className={titleStyle}>your OneUpGaming Account</h4>
