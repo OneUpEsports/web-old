7a7094993cee46fd71f2ae41e9f3db9c63d44e9c
zAlexEU <kitzlex@gmx.at>
2022-01-09 16:21:45 +0100

    socials links added on dashboard

--- src/components/dashb-comps/BottomBar.jsx
+++ src/components/dashb-comps/BottomBar.jsx
@@ -6,11 +6,11 @@ const BottomBar = () => {
     return(
         <div className="flex flex-col lg:flex-row  items-center justify-between pr-4">
             <div className="flex">
-                <Link to="#"  className="px-2"><BsDiscord/></Link>
-                <Link to="#"  className="px-2"><BsTwitter /></Link>
-                <Link to="#"  className="px-2"><BsInstagram /></Link>
-                <Link to="#"  className="px-2"><BsYoutube /></Link>
-                <Link to="#"  className="px-2"><BsTwitch /></Link>
+                <Link to="https://bit.ly/ougdiscord"  className="px-2"><BsDiscord/></Link>
+                <Link to="https://bit.ly/ougtwitter"  className="px-2"><BsTwitter /></Link>
+                <Link to="https://bit.ly/ouginstagram"  className="px-2"><BsInstagram /></Link>
+                <Link to="https://bit.ly/ougyoutube"  className="px-2"><BsYoutube /></Link>
+                <Link to="https://bit.ly/ougtwitch"  className="px-2"><BsTwitch /></Link>
             </div>
            
             <div className=" hidden lg:block pr-6 text-sm">
