de55db9ca9c9f28e7df19f6f4a08d361d0200023
CHENNI-Nidhaleddine <75954258+CHENNI-Nidhaleddine@users.noreply.github.com>
2022-01-10 19:50:33 +0100

    Update BottomBar.jsx

--- src/components/dashb-comps/BottomBar.jsx
+++ src/components/dashb-comps/BottomBar.jsx
@@ -6,11 +6,11 @@ const BottomBar = () => {
     return(
         <div className="flex flex-col lg:flex-row  items-center justify-between pr-4">
             <div className="flex">
-                <Link to="https://bit.ly/ougdiscord"  className="px-2"><BsDiscord/></Link>
-                <Link to="https://bit.ly/ougtwitter"  className="px-2"><BsTwitter /></Link>
-                <Link to="https://bit.ly/ouginstagram"  className="px-2"><BsInstagram /></Link>
-                <Link to="https://bit.ly/ougyoutube"  className="px-2"><BsYoutube /></Link>
-                <Link to="https://bit.ly/ougtwitch"  className="px-2"><BsTwitch /></Link>
+                <Link to="https://bit.ly/ougdiscord"  className="px-4"><BsDiscord className="text-md"/></Link>
+                <Link to="https://bit.ly/ougtwitter"  className="px-4"><BsTwitter className="text-md"/></Link>
+                <Link to="https://bit.ly/ouginstagram"  className="px-4"><BsInstagram className="text-md"/></Link>
+                <Link to="https://bit.ly/ougyoutube"  className="px-4"><BsYoutube className="text-md"/></Link>
+                <Link to="https://bit.ly/ougtwitch"  className="px-4"><BsTwitch className="text-md"/></Link>
             </div>
            
             <div className=" hidden lg:block pr-6 text-sm">
