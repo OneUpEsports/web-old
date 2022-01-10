351ad36e4c555cc3c1c3e2936954d0c80b01f0fd
CHENNI-Nidhaleddine <75954258+CHENNI-Nidhaleddine@users.noreply.github.com>
2022-01-10 20:27:36 +0100

    Update BottomBar.jsx

--- src/components/dashb-comps/BottomBar.jsx
+++ src/components/dashb-comps/BottomBar.jsx
@@ -4,18 +4,18 @@ import {BsDiscord,BsTwitter,BsInstagram,BsYoutube,BsTwitch} from "react-icons/bs
 
 const BottomBar = () => {
     return(
-        <div className="flex flex-col lg:flex-row  items-center justify-between pr-4">
+        <div className="flex flex-col lg:flex-row  items-center justify-between ">
             <div className="flex">
-                <Link to="https://bit.ly/ougdiscord"  className="px-4"><BsDiscord className="text-md"/></Link>
-                <Link to="https://bit.ly/ougtwitter"  className="px-4"><BsTwitter className="text-md"/></Link>
-                <Link to="https://bit.ly/ouginstagram"  className="px-4"><BsInstagram className="text-md"/></Link>
-                <Link to="https://bit.ly/ougyoutube"  className="px-4"><BsYoutube className="text-md"/></Link>
-                <Link to="https://bit.ly/ougtwitch"  className="px-4"><BsTwitch className="text-md"/></Link>
+                <Link to="https://bit.ly/ougdiscord"  className="px-2 mb-4"><BsDiscord/></Link>
+                <Link to="https://bit.ly/ougtwitter"  className="px-2 mb-4"><BsTwitter /></Link>
+                <Link to="https://bit.ly/ouginstagram"  className="px-2 mb-4"><BsInstagram /></Link>
+                <Link to="https://bit.ly/ougyoutube"  className="px-2 mb-4"><BsYoutube /></Link>
+                <Link to="https://bit.ly/ougtwitch"  className="px-2 mb-4"><BsTwitch /></Link>
             </div>
            
             <div className=" hidden lg:block pr-6 text-sm">
                 <span>
-                
+                 
                 </span>
             </div>
       
