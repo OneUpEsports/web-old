36fb4841b081c026002c284f3d618fdbd7452eac
CHENNI-Nidhaleddine <75954258+CHENNI-Nidhaleddine@users.noreply.github.com>
2022-01-14 23:09:45 +0100

    Update Friend.jsx

--- src/components/dashb-comps/Friend.jsx
+++ src/components/dashb-comps/Friend.jsx
@@ -3,11 +3,11 @@ import {FcSettings} from "react-icons/fc"
 
 const Friend = ({image,name,games,title}) => {
     return(
-        <div className="flex  items-center justify-center mb-2 lg:w-full  lg:p-auto  pl-8 w-[calc(740px)]">
+        <div className="flex  items-center justify-center mb-2 lg:w-full  lg:p-auto  pl-8 w-[calc(740px)] ">
         <div className="grid grid-cols-5 grid-flow-col-dense  w-full px-6  overflow-x-scroll   scrollbar-hide">
         
             <div className="flex items-center justify-left pl-6  col-span-1">
-                <img className="h-24 w-24 mr-3" src={image} alt=""/>
+                <img className="h-16 w-16 mr-3" src={image} alt=""/>
                 <div className=" col-span-1"><h3>{name}</h3></div>
             </div>
             
@@ -16,14 +16,14 @@ const Friend = ({image,name,games,title}) => {
             <div className="absolute -top-1 z-50">
                 {title}
             </div>
-            <div className="flex ">
-            <img className="h-8 w-8 mr-1 ml-2" src="/img/create_team/valorant_preview.png" alt=""/>
-            <img className="h-8 w-8 mr-1 ml-2" src="/img/create_team/rocketleague_preview.png" alt=""/>
-            <img className="h-8 w-8 mr-1 ml-2" src="/img/create_team/apex_preview.png" alt=""/>
+            <div className="flex items-center justify-center pt-3">
+            <img className="h-10 w-10 mr-1 ml-2" src="/img/create_team/valorant_preview.png" alt=""/>
+            <img className="h-10 w-10 mr-1 ml-2" src="/img/create_team/rocketleague_preview.png" alt=""/>
+            <img className="h-10 w-10 mr-1 ml-2" src="/img/create_team/apex_preview.png" alt=""/>
             </div>
             </div>
             <div className="flex  items-center justify-center col-span-2">
-            <button className="rounded-xl bg-gray-200  w-24 h-12 text-md "
+            <button className="rounded-xl bg-gray-200  w-24 h-8 text-md "
             >Invite</button>
             </div>
            
