1ceb22d78d08b2cfbf8967becae485e43f0b2146
nid_ch <ne.chenni@gmail.com>
2022-01-16 01:20:39 +0100

    changing-apex-table

--- src/components/tournament/Tournament.jsx
+++ src/components/tournament/Tournament.jsx
@@ -1,21 +1,22 @@
 import React from "react"
 
 
-const Tournament=({date,format,rule,price,slots})=>{
+const Tournament=({date,format,rule,price,up,down,leftNb,rightNb})=>{
     return(
         <div className="flex  items-center justify-center mb-2 lg:w-full w-[calc(540px)]   m-auto  mt-4">
         <div className="grid grid-cols-6 grid-flow-col-dense w-full text-white  ">
         
 
-            <div className="  justify-left"><h4 className="w-fit m-auto">{date}</h4></div>
-            <div className="flex items-center justify-left pl-6 ">
+            <div className=" flex flex-col items-center justify-left"><h4 className="w-fit m-auto font-bold">{date}</h4></div>
+            <div className="flex flex-col items-center justify-left pl-6 ">
+                <img className="w-10 h-10" src="/img/tournament/bracket.png" alt=""/>
                 <h4 className="w-fit m-auto">{format}</h4>
             </div>
-            <div className="flex  items-center justify-center"><h4 className="w-fit m-auto">{rule} </h4></div>
-            <div className="flex  items-center justify-center"><h4 className="w-fit m-auto">{price} </h4></div>
-            <div className="flex  items-center justify-center"><h4 className="w-fit m-auto">{slots} </h4></div>
+            <div className="flex  items-center justify-center"><h4 className=" flex flex-col items-center justify-center w-fit m-auto font-bold">{rule} <span className="block mt-3 flex lg:flex-row flex-col items-center justify-center"><span className=" block rounded-2xl text-black bg-[#b2f617] px-3 py-1 font-bold lg:mr-4">{leftNb}</span>V<span className="block lg:ml-4 rounded-2xl text-black bg-[#b2f617] px-3 py-1 font-bold">{rightNb}</span></span> </h4></div>
+            <div className="flex  items-center justify-center"><h4 className="w-fit m-auto font-bold text-md">{price}<span className="text-sm">c</span> </h4></div>
+            <div className="flex  items-center justify-center"><h4 className="w-fit m-auto"><span className=" font-bold text-md">{up}</span> /{down} </h4></div>
             
-            <div className="flex  items-center justify-center "><button className="text-sm bg-[#2d2928] ">See tournament</button></div>
+            <div className="flex  items-center justify-center "><button className="text-sm bg-[#292d30] px-3 py-3 font-bold">See tournament</button></div>
         </div>
         </div>
     )
--- src/pages/tournaments/crossplay/apex-legends.jsx
+++ src/pages/tournaments/crossplay/apex-legends.jsx
@@ -17,6 +17,15 @@ const CrossPlatForm=()=>{
                 </div>
             </div>
             <div className=" relative z-30 lg:w-5/6 m-auto bg-[#1c1a1b] h-fit mt-0 pt-0 w-full overflow-x-scroll scrollbar-hide">
+                         <div className="grid grid-cols-6 grid-flow-col-dense lg:w-full  w-[calc(540px)] m-auto">
+                             <div className="flex intems-center justify-center bg-[#b2f617] col-span-2 ">
+                                 <h3 calssName="w-fit m-auto">TODAY</h3>
+                             </div>
+                             <div className="bg-[#292d30] col-span-4 ">
+                               
+                             </div>
+
+                         </div>
                         <div className="grid grid-cols-6 grid-flow-col-dense lg:w-full  w-[calc(540px)] m-auto text-white  bg-[#2d2928]  ">
                                 <div className="  justify-left"><h3 className="w-fit m-auto">Time</h3></div>
                                 <div className="flex items-center justify-left pl-6 ">
@@ -28,14 +37,42 @@ const CrossPlatForm=()=>{
                                 
                                 <div className="flex  items-center justify-center "><button className="text-sm bg-gray-200"></button></div>
                         </div>
-                <Tournament date="12.03.2022 9:00" format="Bracket" rule="1vs1 cup" price="2 EUR" slots="24/32"/>
-                <Tournament date="12.03.2022 9:00" format="Bracket" rule="1vs1 cup" price="2 EUR" slots="24/32"/>
-                <Tournament date="12.03.2022 9:00" format="Bracket" rule="1vs1 cup" price="2 EUR" slots="24/32"/>
-                <Tournament date="12.03.2022 9:00" format="Bracket" rule="1vs1 cup" price="2 EUR" slots="24/32"/>
-                <Tournament date="12.03.2022 9:00" format="Bracket" rule="1vs1 cup" price="2 EUR" slots="24/32"/>
-                <Tournament date="12.03.2022 9:00" format="Bracket" rule="1vs1 cup" price="2 EUR" slots="24/32"/>
-                <Tournament date="12.03.2022 9:00" format="Bracket" rule="1vs1 cup" price="2 EUR" slots="24/32"/>
-               
+                <Tournament date="3:00 PM" format="Bracket" rule="1vs1 Saturday damage cup" price="50" up="43" down="64" leftNb="1" rightNb="1"/>
+                <Tournament date="6:00 PM" format="Bracket" rule="Apex Legends Top damage" price="3"  up="12" down="32" leftNb="9" rightNb="7"/>
+                <Tournament date="9:00 PM" format="Bracket" rule="Apex Legends Top damage" price="3"  up="6" down="32" leftNb="1" rightNb="1"/>
+                
+                      <div className="grid grid-cols-6 grid-flow-col-dense lg:w-full  w-[calc(540px)] m-auto">
+                             <div className="flex intems-center justify-center bg-[#b2f617] col-span-2 ">
+                                 <h3 calssName="w-fit m-auto">TOMORROW</h3>
+                             </div>
+                             <div className="bg-[#292d30] col-span-4 ">
+                               
+                             </div>
+
+                         </div>
+                        <div className="grid grid-cols-6 grid-flow-col-dense lg:w-full  w-[calc(540px)] m-auto text-white  bg-[#2d2928]  ">
+                                <div className="  justify-left"><h3 className="w-fit m-auto">Time</h3></div>
+                                <div className="flex items-center justify-left pl-6 ">
+                                    <h3 className="w-fit m-auto">Format</h3>
+                                </div>
+                                <div className="flex  items-center justify-center"><h3 className="w-fit m-auto">Rule </h3></div>
+                                <div className="flex  items-center justify-center"><h3 className="w-fit m-auto">Price Money </h3></div>
+                                <div className="flex  items-center justify-center"><h3 className="w-fit m-auto">Slots </h3></div>
+                                
+                                <div className="flex  items-center justify-center "><button className="text-sm bg-gray-200"></button></div>
+                        </div>
+                        <Tournament date="3:00 PM" format="Bracket" rule="Apex Legends Top damage" price="30"  up="24" down="32" leftNb="1" rightNb="1"/>
+                <Tournament date="6:00 PM" format="Bracket" rule="Apex Legends Top damage" price="33" up="24" down="32" leftNb="1" rightNb="1"/>
+                <Tournament date="9:00 PM" format="Bracket" rule="Apex Legends Top damage" price="3" up="24" down="32" leftNb="1" rightNb="1"/>
+                <div className="grid grid-cols-6 grid-flow-col-dense w-full m-auto">
+                             <div className="flex intems-center justify-center bg-[#b2f617] col-span-2 ">
+                                 <h3 calssName="w-fit m-auto">MONDAY,JANUARY 17</h3>
+                             </div>
+                             <div className="bg-[#292d30] col-span-4 ">
+                               
+                             </div>
+
+                         </div>
 
             </div>
         </div>
