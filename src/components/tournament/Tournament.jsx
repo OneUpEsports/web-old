81b14fb886ffe9e5c648b3d93d922a66bb8b5f8e
CHENNI-Nidhaleddine <75954258+CHENNI-Nidhaleddine@users.noreply.github.com>
2022-01-19 23:40:34 +0100

    Update Tournament.jsx

--- src/components/tournament/Tournament.jsx
+++ src/components/tournament/Tournament.jsx
@@ -24,7 +24,7 @@ const Tournament=({date,format,rule,price,up,down,leftNb,rightNb,startin})=>{
                 <h4 className="w-fit m-auto">{format}</h4>
             </div>
             <div className="flex  items-center justify-center"><h4 className=" flex flex-col items-center justify-center w-fit m-auto font-bold"><span className="md:block hidden">{rule}</span> <span className=" mt-3 flex  items-center justify-center"><span className="  rounded-2xl text-black bg-[#b154f0] px-3 py-1 font-bold lg:mr-4">{leftNb}</span>V<span className=" lg:ml-4 rounded-2xl text-black bg-[#b154f0] px-3 py-1 font-bold">{rightNb}</span></span> </h4></div>
-            <div className="flex  items-center justify-center"><h4 className="w-fit m-auto font-bold text-md">{price}<span className="text-sm">c</span> </h4></div>
+            <div className="flex  items-center justify-center"><h4 className="w-fit m-auto font-bold text-md">{price}<span className="text-sm">€</span> </h4></div>
             <div className="flex  items-center justify-center"><h4 className="w-fit m-auto"><span className=" font-bold text-md">{up}</span> /{down} </h4></div>
             
             <div className="flex  items-center justify-center hidden md:block "><button className="text-sm bg-[#292d30] px-3 py-3 font-bold">See tournament</button></div>
