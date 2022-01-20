030b58100ef55597e5f376a21e0600dce59ec686
zAlexEU <kitzlex@gmx.at>
2022-01-20 19:29:35 +0100

    king

--- src/pages/dashboard/games.jsx
+++ src/pages/dashboard/games.jsx
@@ -12,12 +12,12 @@ const Trophies=()=>{
                 
                 <div className=" h-full  w-11/12 m-auto grid lg:grid-cols-4 lg:grid-rows-3  md:grid-cols-2 md:grid-rows-7 grid-cols-1 grid-rows-11 lg:gap-x-12  gap-y-6 ">
                       <MultiPlatGame image="/img/dashboard/NoPath - Kopie (3).png" name="APEX LEGENDS" pclink="/tournaments/crossplay/apex-legends" pslink="/tournaments/crossplay/apex-legends" xboxlink="/tournaments/crossplay/apex-legends"/>
-                      <MobileGame image="/img/dashboard/27783djg.png" name="CLASH OF CLANS"/>
-                      <MobileGame image="/img/dashboard/NoPath - Kopie (5).png" name="CALL OF DUTY MOBILE"/>
+                      <MobileGame image="/img/dashboard/27783djg.png" name="CLASH OF CLANS" mobilelink="/tournaments/mobile/clash-of-clans"/> 
+                      <MobileGame image="/img/dashboard/NoPath - Kopie (5).png" name="CALL OF DUTY MOBILE" mobilelink="/tournaments/mobile/call-of-duty"/>
                       <MultiPlatGame image="/img/dashboard/lsu1ddvx.png" name="CALL OF DUTY WARZONE" pclink="/tournaments/crossplay/warzone" pslink="/tournaments/crossplay/warzone" xboxlink="/tournaments/crossplay/warzone"/>
                       <MultiPlatGame image="/img/dashboard/NoPath - Kopie (7).png" name="FIFA 22"/>
                       <MultiPlatGame image="/img/dashboard/NoPath - Kopie (10).png" name="FORTNITE" pclink="/tournaments/crossplay/fortnite" pslink="/tournaments/crossplay/fortnite" xboxlink="/tournaments/crossplay/fortnite"/>
-                      <PcGame image="/img/dashboard/NoPath - Kopie (12).png" name="LEAGUE OF LEGENDS"/>
+                      <PcGame image="/img/dashboard/NoPath - Kopie (12).png" name="LEAGUE OF LEGENDS" pclink="/tournaments/pc/league-of-legends"/>
                       <MultiPlatGame image="/img/dashboard/NoPath - Kopie (16).png" name="RAINBOW SIX SIEGE"/>
                       <MultiPlatGame image="/img/dashboard/NoPath - Kopie (18).png" name="ROCKET LEAGUE" pclink="/tournaments/crossplay/rocket-league" pslink="/tournaments/crossplay/rocket-league" xboxlink="/tournaments/crossplay/rocket-league"/>
                       <PcGame image="/img/dashboard/NoPath - Kopie (20).png" name="VALORANT" pclink="/tournaments/pc/valorant"/>
