407a96adbd13d9fa2b226b5d030d75e131e74a71
zAlexEU <kitzlex@gmx.at>
2022-01-21 08:32:16 +0100

    .

--- src/pages/dashboard/games.jsx
+++ src/pages/dashboard/games.jsx
@@ -12,16 +12,16 @@ const Trophies=()=>{
                 
                 <div className=" h-full  w-11/12 m-auto grid lg:grid-cols-4 lg:grid-rows-3  md:grid-cols-2 md:grid-rows-7 grid-cols-1 grid-rows-11 lg:gap-x-12  gap-y-6 ">
                       <MultiPlatGame image="/img/dashboard/NoPath - Kopie (3).png" name="APEX LEGENDS" pclink="/tournaments/crossplay/apex-legends" pslink="/tournaments/crossplay/apex-legends" xboxlink="/tournaments/crossplay/apex-legends"/>
-                      <MobileGame image="/img/dashboard/27783djg.png" name="CLASH OF CLANS" link="/mobile/clash-of-clans"/>
-                      <MobileGame image="/img/dashboard/NoPath - Kopie (5).png" name="CALL OF DUTY MOBILE" link="/mobile/call-of-duty"/>
-                      <MultiPlatGame image="/img/dashboard/lsu1ddvx.png" name="CALL OF DUTY WARZONE" pclink="/crossplay/call-of-duty-warzone" pslink="/crossplay/call-of-duty-warzone" xboxlink="/crossplay/call-of-duty-warzone"/>
-                      <MultiPlatGame image="/img/dashboard/NoPath - Kopie (7).png" name="FIFA 22" pclink="/crossplay/fifa22" pslink="/crossplay/fifa22" xboxlink="/crossplay/fifa22"/>
-                      <MultiPlatGame image="/img/dashboard/NoPath - Kopie (10).png" name="FORTNITE" pclink="/crossplay/fortnite" pslink="/crossplay/fortnite" xboxlink="/crossplay/fortnite"/>
-                      <PcGame image="/img/dashboard/NoPath - Kopie (12).png" name="LEAGUE OF LEGENDS" link="/pc/league-of-legends"/>
-                      <MultiPlatGame image="/img/dashboard/NoPath - Kopie (16).png" name="RAINBOW SIX SIEGE" pclink="/crossplay/rainbow-six-siege" pslink="/crossplay/rainbow-six-siege" xboxlink="/crossplay/rainbow-six-siege"/>
-                      <MultiPlatGame image="/img/dashboard/NoPath - Kopie (18).png" name="ROCKET LEAGUE" pclink="/crossplay/rocket-league" pslink="/crossplay/rocket-league" xboxlink="/crossplay/rocket-league"/>
-                      <PcGame image="/img/dashboard/NoPath - Kopie (20).png" name="VALORANT" link="/pc/valorant"/>
-                      <PcGame image="/img/dashboard/NoPath - Kopie (21).png" name="CSGO" link="/pc/csgo"/>
+                      <MobileGame image="/img/dashboard/27783djg.png" name="CLASH OF CLANS"/>
+                      <MobileGame image="/img/dashboard/NoPath - Kopie (5).png" name="CALL OF DUTY MOBILE"/>
+                      <MultiPlatGame image="/img/dashboard/lsu1ddvx.png" name="CALL OF DUTY WARZONE"/>
+                      <MultiPlatGame image="/img/dashboard/NoPath - Kopie (7).png" name="FIFA 22"/>
+                      <MultiPlatGame image="/img/dashboard/NoPath - Kopie (10).png" name="FORTNITE"/>
+                      <PcGame image="/img/dashboard/NoPath - Kopie (12).png" name="LEAGUE OF LEGENDS"/>
+                      <MultiPlatGame image="/img/dashboard/NoPath - Kopie (16).png" name="RAINBOW SIX SIEGE"/>
+                      <MultiPlatGame image="/img/dashboard/NoPath - Kopie (18).png" name="ROCKET LEAGUE"/>
+                      <PcGame image="/img/dashboard/NoPath - Kopie (20).png" name="VALORANT"/>
+                      <PcGame image="/img/dashboard/NoPath - Kopie (21).png" name="CSGO"/>
                       
                     
                 </div>
