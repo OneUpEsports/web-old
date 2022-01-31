a3bc903797d76fbd9f3170deabe6f6395f1b5006
AbdulWajid111 <awajidkhan50@gmail.com>
2022-01-31 16:56:27 +0500

    footer removed

--- src/components/tournamnet-details/footer/footer.jsx
+++ src/components/tournamnet-details/footer/footer.jsx
@@ -3,25 +3,6 @@ const Footer = () => {
   return (
     <div>
       <div>
-        {/*footer_up */}
-        <div className="footer_up">
-          <div className="flex justify-center">
-            <div className="w-4/5 py-7 px-2.5">
-              {/* heading */}
-              <div>
-                <h4 className="text-white text-base font-bold">
-                  PRIVACY & LEGAL
-                </h4>
-              </div>
-              <ul className="main_ul text-sm font-normal pb-2">
-                <li className="text-sm font-normal pb-1">Legal notice</li>
-                <li className="text-sm font-normal pb-1">Terms of use</li>
-                <li className="text-sm font-normal pb-1">Privacy policy</li>
-                <li className="text-sm font-normal pb-1">Cookies policy</li>
-              </ul>
-            </div>
-          </div>
-        </div>
         {/* down_footer */}
         <div className="down_footer">
           {/* text_div */}
