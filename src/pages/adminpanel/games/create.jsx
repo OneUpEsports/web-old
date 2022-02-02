cbd7cae9fb060b89ba85327ec410dc62cca9238c
nid_ch <ne.chenni@gmail.com>
2022-02-02 23:39:34 +0100

    adding-line-breaks

--- src/pages/adminpanel/games/create.jsx
+++ src/pages/adminpanel/games/create.jsx
@@ -46,7 +46,7 @@ const Home=()=>{
                                <div>
                                 <h3 className="font-bold">PlattForm</h3>
                                 <input className="mr-2 h-5 w-5" type="checkbox" id="crossplay" name="crossplay" value="Crossplay"/>
-                                <label className="mr-4" for="crossplay">Crossplay</label>
+                                <label className="mr-4" for="crossplay">Crossplay</label><br/>
                                 <input className="mr-2 h-5 w-5 " type="checkbox" id="playstation" name="playstation" value="playstation"/>
                                 <label for="playstation">playstation</label>
                                 <input className="mr-2 h-5 w-5 " type="checkbox" id="ps4/5" name="ps4/5" value="PS4/5"/>
@@ -54,9 +54,9 @@ const Home=()=>{
                                 <input className="mr-2 h-5 w-5 " type="checkbox" id="xbox" name="xbox" value="xbox"/>
                                 <label for="xbox">xbox</label>
                                 <input className="mr-2 h-5 w-5" type="checkbox" id="xb/xbx" name="xb/xbx" value="XB/XBX"/>
-                                <label className="mr-4" for="xb/xbx">xbox/xbox-series</label>
+                                <label className="mr-4" for="xb/xbx">xbox/xbox-series</label><br/>
                                 <input className="mr-2 h-5 w-5 " type="checkbox" id="mobile" name="mobile" value="Mobile"/>
-                                <label className="mr-4" for="mobile">Mobile</label>
+                                <label className="mr-4" for="mobile">Mobile</label><br/>
                                 <input className="mr-2 h-5 w-5 " type="checkbox" id="pc" name="pc" value="Pc"/>
                                 <label for="pc">Pc</label>
                                 </div>
