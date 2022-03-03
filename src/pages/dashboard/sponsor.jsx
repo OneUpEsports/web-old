888444a124dbd5b1e5fe0f9005fc6721d471fe88
zAlexEU <kitzlex@gmx.at>
2022-03-03 13:01:38 +0100

    sponsor added

---
+++ src/pages/dashboard/sponsor.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { Helmet } from "react-helmet";
+import { graphql } from "gatsby";
+
+const News = () => {
+    const titleStyle = "text-center  mb-2";
+    const containerStyle =
+        " max-w-xl  text-white m-auto my-auto text-center justify-items-center  justify-center items-center";
+
+    return (
+        <>
+            <Helmet title="OneUpGaming • News" defer={false} />
+            <div className=" absolute bottom-0 right-0 bg-black m-0 h-full w-full  items-center mb-0 pb-0 py-24  overflow-x-hidden overflow-y-scroll">
+                <div className={containerStyle}>
+                    <h3 className={titleStyle}>Sponsor</h3>
+                </div>
+                <h4 className={containerStyle}>Code goes here</h4>
+            </div>
+        </>
+    );
+};
+
+export const query = graphql`
+    query newsPageQuery {
+        allGeneral {
+            nodes {
+                section
+                id
+                menu {
+                    ...Menu
+                }
+                footer {
+                    ...Footer
+                }
+            }
+        }
+        page(title: { eq: "sponsorPage" }, pageType: { eq: innerpage }) {
+            content {
+                ...PageContentAll
+            }
+        }
+    }
+`;
+
+export default Sponsor;
